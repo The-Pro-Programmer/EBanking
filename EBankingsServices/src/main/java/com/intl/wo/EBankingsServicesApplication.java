65600dd480b816cad649b4c91767b5f7f99c4aa9
Mansi Wakhankar <mansiswakharkar@gmail.com>
2020-07-25 19:34:52 +0530

    Initial commit

---
+++ EBankingsServices/src/main/java/com/intl/wo/EBankingsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.intl.wo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankingsServicesApplication.class, args);
+	}
+
+}
---
+++ EBankingsServices/src/test/java/com/intl/wo/EBankingsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intl.wo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
